5c3e56b30dd53f6c4ba74ead0a9fa87672c99655
motojate <motojate@naver.com>
2023-07-02 21:49:45 +0900

    * schedule 용 store 추가

---
+++ src/stores/member-schedule-store.ts
@@ -0,0 +1,26 @@
+import { defineStore } from 'pinia'
+import { FIND_SCHEDULE_FROM_USER_AND_MONTH } from 'src/graphql/member-schedule'
+import { useQuery } from 'villus'
+import { ref } from 'vue'
+
+export const useMemberScheduleStore = defineStore('member-schedule', () => {
+  const memberUserSchedule = ref()
+
+  const findScheduleFromUserAndMonth = async (month: number) => {
+    const { data } = await useQuery({
+      query: FIND_SCHEDULE_FROM_USER_AND_MONTH,
+      variables: {
+        month: month,
+      },
+      cachePolicy: 'network-only',
+    })
+    memberUserSchedule.value = data?.value?.selectCharacter
+  }
+
+  const state = { result }
+  const action = {
+    selectCharacter,
+  }
+
+  return { ...state, ...action }
+})
