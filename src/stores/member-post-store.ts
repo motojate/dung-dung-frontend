71934b7f4b65646d433d4799d3275997a5a735f5
motojate <motojate@naver.com>
2023-09-05 23:30:35 +0900

    * 게시글 관련된 스토어 초기 셋팅

---
+++ src/stores/member-post-store.ts
@@ -0,0 +1,8 @@
+import { defineStore } from 'pinia'
+
+export const userMemberPostStore = defineStore('member-post', () => {
+  const state = {}
+  const action = {}
+
+  return { ...state, ...action }
+})
