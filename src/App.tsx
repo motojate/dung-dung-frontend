7d2db5d6de2905fad8bb831282887f3cb747cf80
motojate <motojate@naver.com>
2024-06-19 23:48:18 +0900

    [FEAT] 메인 화면에 달력 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import RouterConfig from './routes';
 import './App.css';
 import useAppSelector from './hooks/useAppSelector';
-import { CircularProgress } from '@mui/material';
 
 const App = () => {
   const isLoading = useAppSelector(state => state.loadingReducer.isLoading);
--- src/pages/HomeMain.tsx
+++ src/pages/HomeMain.tsx
@@ -1,12 +1,37 @@
 import { Box, Typography } from '@mui/material';
 import MyAnimalCard from 'src/components/cards/MyAnimalCard';
 import TodayTodoCard from 'src/components/cards/TodayTodoCard';
+import 'react-calendar/dist/Calendar.css';
+import Calendar from 'react-calendar';
+import { useState } from 'react';
+import { TileArgs, Value } from 'react-calendar/dist/cjs/shared/types';
+import styles from 'src/modules/Calendar.module.css';
 
 const HomeMain = () => {
+  const [value, setValue] = useState<Date>(new Date());
+
+  const onChange = (value: Value) => {
+    if (value instanceof Date) setValue(value);
+  };
+
+  const titleClassName = ({ date, view }: TileArgs) => {
+    if (view === 'month') {
+      if (date.getDay() === 0) return styles.sundayHeader;
+      if (date.getDay() === 6) return styles.saturdayBody;
+    }
+  };
+
   return (
     <>
-      <MyAnimalCard />
-      <TodayTodoCard />
+      <Calendar
+        onChange={onChange}
+        value={value}
+        calendarType="gregory"
+        showNeighboringMonth={false}
+        tileClassName={titleClassName}
+      />
+      {/* <MyAnimalCard />
+      <TodayTodoCard /> */}
     </>
   );
 };
